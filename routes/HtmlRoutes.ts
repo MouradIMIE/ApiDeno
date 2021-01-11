b41963c369fe4ca9d9f43ce2c682545254099996
MouradBoukhalfi <mouradboukhalfi3@gmail.com>
2021-01-11 19:49:29 +0100

    finish routehtml

--- routes/HtmlRoutes.ts
+++
@@ -1,9 +0,0 @@
-import {HtmlController} from '../controllers/HtmlController.ts'
-import { opine } from "https://deno.land/x/opine@1.0.2/mod.ts";
-import { Application } from "https://deno.land/x/opine@1.0.2/src/types.ts";
-
-const route: Application = opine();
-
-route.get('/', HtmlController.getHtml);
-
-export { route as HtmlRoute }
